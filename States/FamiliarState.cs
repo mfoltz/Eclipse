37ca62da8a770cd7a8536c234ad6838c80a083a1
zfolmt <mitchell.foltz@gmail.com>
2025-07-23 21:05:38 -0500

    Add state classes for UI elements

---
+++ States/FamiliarState.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Eclipse.States;
+
+internal class FamiliarState
+{
+    internal float Progress { get; set; }
+    internal int Level { get; set; } = 1;
+    internal int Prestige { get; set; }
+    internal int MaxLevel { get; set; } = 90;
+    internal string Name { get; set; } = string.Empty;
+    internal List<string> Stats { get; set; } = new() { "", "", "" };
+}
